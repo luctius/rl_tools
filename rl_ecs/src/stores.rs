d9c720335c7ce2fce3d24ad42179a39a79bc1583
Luctius <luctius@gmail.com>
2021-08-22 10:01:57 +0200

    cargo fmt

--- rl_ecs/src/stores.rs
+++ rl_ecs/src/stores.rs
@@ -71,7 +71,9 @@ impl<PKEY, CKEY, S> StoreExAttach<PKEY, CKEY> for S
 {
     fn attach(&mut self, pkey: PKEY, ckey: CKEY) -> bool { self.set_parent(ckey, pkey) && self.set_child(pkey, ckey) }
 
-    fn detach(&mut self, pkey: PKEY, ckey: CKEY) -> bool { self.clear_parent(ckey, pkey) && self.clear_child(pkey, ckey) }
+    fn detach(&mut self, pkey: PKEY, ckey: CKEY) -> bool {
+        self.clear_parent(ckey, pkey) && self.clear_child(pkey, ckey)
+    }
 }
 
 pub trait StoreExCreateAttach<T, PKEY, CKEY>
